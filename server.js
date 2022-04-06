1bbf122cf6eb061479890d0d8778606623b7cc33
mariaclarabez <mariaclarabez6@gmail.com>
2022-04-06 02:10:07 -0400

    first commit

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 import express from 'express';
+import cors from 'cors';
 import helloController
     from "./controllers/hello-controller.js";
-import userController   from "./controllers/user-controller.js";
+import usersController   from "./controllers/users-controller.js";
+import tuitsController from "./controllers/tuits-controller.js";
 
 const app = express();
+app.use(express.json());
+app.use(cors());
+
 helloController(app);
-userController(app);
+usersController(app);
+tuitsController(app);
 app.listen(4000);
